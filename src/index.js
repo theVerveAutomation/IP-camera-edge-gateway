991ca99408b509de3842e2d2391b725f282b8628
kalpa perera <163730747+klperera@users.noreply.github.com>
2026-01-12 14:43:40 +0530

    feat: enhance streamCamera function with improved argument handling and logging for camera names

--- src/index.js
+++ src/index.js
@@ -5,28 +5,36 @@ require('dotenv').config();
 // Example function to pull RTSP and push to cloud
 function streamCamera(camera_name, rtspUrl, cloudUrl) {
     // const cmd = `ffmpeg -hide_banner -rtsp_transport tcp -i ${rtspUrl} -c copy output${camera_name}.mp4`;
-    const args = ['-hide_banner', '-rtsp_transport', 'tcp', '-i', rtspUrl, '-c', 'copy', `output${camera_name.trim()}.mp4`];
+    const args = [
+        '-hide_banner',
+        '-rtsp_transport', 'tcp',
+        '-i', rtspUrl,
+        '-vcodec', 'libx264', //-vcodec h264_vaapi (Intel GPU), -vcodec h264_nvenc (NVIDIA GPU)
+        '-preset', 'ultrafast', '-tune', 'zerolatency',
+        '-vf', 'scale=1280:720,fps=25',
+        '-c', 'copy', `output${camera_name.trim()}.mp4`
+    ];
     const ffmpeg = spawn('ffmpeg', args, { stdio: ['ignore', 'pipe', 'pipe'] });
     console.log(`Executing command for ${camera_name}:  ${'ffmpeg ' + args.join(' ')}`);
 
     ffmpeg.stdout.on('data', (data) => {
-        console.log(`STDOUT [${camera_name}]: ${data}`);
+        console.log(`STDOUT [${camera_name.trim()}]: ${data}`);
     });
 
     ffmpeg.stderr.on('data', (data) => {
-        console.log(`STDERR [${camera_name}]: ${data}`);
+        console.log(`STDERR [${camera_name.trim()}]: ${data}`);
     })
 
     ffmpeg.on('close', (code) => {
-        console.log(`FFMPEG process for ${camera_name} exited with code ${code}`);
+        console.log(`FFMPEG process for ${camera_name.trim()} exited with code ${code}`);
     });
 
     ffmpeg.on('error', (err) => {
-        console.error(`Failed to start FFMPEG process for ${camera_name}: ${err}`);
+        console.error(`Failed to start FFMPEG process for ${camera_name.trim()}: ${err}`);
     });
 
     ffmpeg.on('exit', (code, signal) => {
-        console.log(`FFMPEG process for ${camera_name} exited with code ${code} and signal ${signal}`);
+        console.log(`FFMPEG process for ${camera_name.trim()} exited with code ${code} and signal ${signal}`);
     });
 }
 

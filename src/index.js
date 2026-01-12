ae5dd92df42c7611ef3e75d663f18b2e25596af0
kalpa perera <163730747+klperera@users.noreply.github.com>
2026-01-12 13:36:47 +0530

    feat: update streamCamera function to include camera name and modify command for RTSP streaming

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ require('dotenv').config();
 
 
 // Example function to pull RTSP and push to cloud
-function streamCamera(rtspUrl, cloudUrl) {
-    const cmd = `ffmpeg -hide_banner -i ${rtspUrl} -f flv ${cloudUrl}`;
-    console.log("Executing command: " + cmd);
+function streamCamera(camera_name, rtspUrl, cloudUrl) {
+    const cmd = `ffmpeg -hide_banner -rtsp_transport tcp -i ${rtspUrl} -c capy output${camera_name}.mp4`;
+    console.log(`Executing command for ${camera_name}:  ${cmd}`);
     const process = exec(cmd);
     process.stdout.on('data', data => console.log("stdout output: " + data));
     process.stderr.on('data', data => console.error("stderr error: " + data));
-    process.on('exit', code => console.log(`Process exited with code ${code}`));
+    process.on('exit', code => console.log(`Process exited with code ${code} `));
     process.on('error', err => console.error("Process error: " + err));
-    process.on('close', code => console.log(`Process closed with code ${code}`));
+    process.on('close', code => console.log(`Process closed with code ${code} `));
 }
 
 
@@ -20,6 +20,6 @@ const cameras_json = process.env.CAMERAS_JSON; // Use Cameras JSON from .env fil
 
 
 for (const camers of JSON.parse(cameras_json)) {
-    streamCamera(camers.rtsp_url, camers.cloud_url);
+    streamCamera(camers.name, camers.rtsp_url, camers.cloud_url);
 }
 // streamCamera(rtspUrl, cloudUrl);

f5cfd251344d1f6123155a43d7e755be9af78858
kalpa perera <163730747+klperera@users.noreply.github.com>
2026-01-13 10:08:57 +0530

    feat: add axios dependency, implement stream management and health check, and configure Docker setup

--- src/index.js
+++ src/index.js
@@ -1,49 +1,19 @@
-const { spawn } = require('child_process');
-require('dotenv').config();
+const { addStream } = require('./stream-manager');
+const config = require('./config');
+const { checkGo2rtcHealth } = require('./utils/healthCheck');
 
+async function startEdgeAgent() {
+    console.log('Starting Edge Gateway Agent...');
+    console.log(`Loaded : ${config.camerasJson}`);
+    for (const cam of config.camerasJson) {
+        await addStream(cam.name, cam.rtsp_url || cam.host);
+    }
 
-// Example function to pull RTSP and push to cloud
-function streamCamera(camera_name, rtspUrl, cloudUrl) {
-    // const cmd = `ffmpeg -hide_banner -rtsp_transport tcp -i ${rtspUrl} -c copy output${camera_name}.mp4`;
-    const args = [
-        '-hide_banner',
-        '-rtsp_transport', 'tcp',
-        '-i', rtspUrl,
-        '-vcodec', 'libx264', //-vcodec h264_vaapi (Intel GPU), -vcodec h264_nvenc (NVIDIA GPU)
-        '-preset', 'ultrafast', '-tune', 'zerolatency',
-        '-vf', 'scale=1280:720,fps=25',
-        '-an', '-f', 'flv', cloudUrl  // c copy output.mp4
-    ];
-    const ffmpeg = spawn('ffmpeg', args, { stdio: ['ignore', 'pipe', 'pipe'] });
-    console.log(`Executing command for ${camera_name}:  ${'ffmpeg ' + args.join(' ')}`);
-
-    ffmpeg.stdout.on('data', (data) => {
-        console.log(`STDOUT [${camera_name.trim()}]: ${data}`);
-    });
-
-    ffmpeg.stderr.on('data', (data) => {
-        console.log(`STDERR [${camera_name.trim()}]: ${data}`);
-    })
-
-    ffmpeg.on('close', (code) => {
-        console.log(`FFMPEG process for ${camera_name.trim()} exited with code ${code}`);
-    });
-
-    ffmpeg.on('error', (err) => {
-        console.error(`Failed to start FFMPEG process for ${camera_name.trim()}: ${err}`);
-    });
-
-    ffmpeg.on('exit', (code, signal) => {
-        console.log(`FFMPEG process for ${camera_name.trim()} exited with code ${code} and signal ${signal}`);
-    });
+    // Health monitoring
+    setInterval(async () => {
+        const healthy = await checkGo2rtcHealth();
+        if (!healthy) console.error('go2rtc is down!');
+    }, 10000);
 }
 
-
-// Example usage
-const cameras_json = process.env.CAMERAS_JSON; // Use Cameras JSON from .env file
-
-
-for (const camers of JSON.parse(cameras_json)) {
-    streamCamera(camers.name, camers.rtsp_url, camers.cloud_url);
-}
-// streamCamera(rtspUrl, cloudUrl);
+startEdgeAgent();
